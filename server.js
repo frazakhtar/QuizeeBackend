02e044df7ee8ee667b4e18b827205e1ec4934a7c
vikram kumar <vikram.kumar2013@gmail.com>
2024-04-05 01:30:10 +0530

    completed save, fetch, update and delete questions API

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ let cors = require("cors");
 let bodyParser = require("body-parser");
 let morgan = require("morgan");
 let mongoose = require("mongoose");
-
+let config = require("./config/config");
 let app = express();
 
-
-let config = require('./config/config');
-let UserRoute = require('./routes/userRoute');
-
+let UserRoute = require("./routes/userRoute");
+let QuestionsRoute = require("./routes/questionsRoute");
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -17,30 +15,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use()
 
-
-
-app.use('/api', UserRoute);
-
+app.use("/api", UserRoute);
+app.use("/api", QuestionsRoute);
 
 app.use((data, req, res, next) => {
-    const error = new Error(data);
-    error.status = 404;
-    next(error);
+  const error = new Error(data);
+  error.status = 404;
+  next(error);
 });
 
 app.listen(config.port, (err) => {
-    if(err){
-        console.log('error occur in starting the server');
-    } else {
-        console.log(`Server started at port ${config.port}`);
-    }
+  if (err) {
+    console.log("error occur in starting the server");
+  } else {
+    console.log(`Server started at port ${config.port}`);
+  }
 });
 
 mongoose.connect(config.database);
-mongoose.connection.on('connected', (err) =>{
-    if(err){
-        console.log('Error in connecting database');
-    } else {
-        console.log('Connected to database at port 27017');
-    }
+mongoose.connection.on("connected", (err) => {
+  if (err) {
+    console.log("Error in connecting database");
+  } else {
+    console.log("Connected to database at port 27017");
+  }
 });
